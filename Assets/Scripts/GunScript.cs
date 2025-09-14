c5300d2a72f7a3c3b5ef2fe7b94e111892880283
zPrisim <76958083+zPrisim@users.noreply.github.com>
2025-09-15 00:45:19 +0200

    Tests Cible + Raycast

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public Transform FirePoint;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(FirePoint.position, transform.TransformDirection(Vector3.forward), out hit, 100))
+            {
+                Debug.DrawRay(FirePoint.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+                hit.collider.SendMessage("HitByBullet", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
